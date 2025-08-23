9b7adacd22b4f77efeb852f9de186bf6d288ba23
Ilya Nikiforov <propandamen@gmail.com>
2025-08-23 14:53:40 +0300

    fix database engine

--- database/base.py
+++ database/base.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
